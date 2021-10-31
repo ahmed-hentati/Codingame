3eaed4497da17728c86c0c7aaef307e00773c3c7
Ahmed Hentati <mister.bouhmid@gmail.com>
2021-10-31 03:48:37 +0100

    add smallest interval puzzle edit

--- FindSmallestInterval/FindSmallestInterval.js
+++ FindSmallestInterval/FindSmallestInterval.js
@@ -5,17 +5,9 @@ const unique = (value, index, self) => {
 function FindSmallestInterval(numbers) {
     let result = numbers.sort().filter(unique);
     let interval = result.length;
-    let loop = 0;
     for ( i = 0; i < interval; i++ ) {
         interval = result[i+1] - result[i];
     }
-    /*while( interval !== 1 && loop !== interval) {
-        loop++;
-        console.log(loop);
-        result.forEach((element, i) => {
-            interval = element[i+1] - element[i];         
-        });
-    }*/
     return interval;
 
 }
